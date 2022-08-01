6a15101c2033fec4038f90da377ac103dc31234a
Yurkes2020 <yurkes2020@gmail.com>
2022-08-01 14:22:15 +0300

    1 test

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngridient = document.querySelector('#ingredients');
+ingredients.map((item) => {
+  const li = document.createElement('li');
+  li.textContent += item;
+  listIngridient.append(li);
+  li.classList.add('item');
+});
