4875c17cdb9b838f29f150a89ce57b0164b485db
Yurkes2020 <yurkes2020@gmail.com>
2022-08-19 12:17:47 +0300

    ))))))))))

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+buttonRef.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+});
